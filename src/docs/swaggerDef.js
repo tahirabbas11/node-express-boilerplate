d1b2ddab9fe23842373392b9329d18d21c4c5bd4
tahirabbas001 <tahir.abbas@riksof.com>
2024-06-11 01:00:46 +0500

    updates

--- src/docs/swaggerDef.js
+++ src/docs/swaggerDef.js
@@ -8,7 +8,7 @@ const swaggerDef = {
     version,
     license: {
       name: 'MIT',
-      url: 'https://github.com/hagopj13/node-express-boilerplate/blob/master/LICENSE',
+      url: 'https://github.com/tahirabbas11/node-express-boilerplate/blob/master/LICENSE',
     },
   },
   servers: [
